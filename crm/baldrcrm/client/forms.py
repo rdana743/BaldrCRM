164d4591e218d78d9a1689914487c0611c7f22e0
rdana743 <116306325+rdana743@users.noreply.github.com>
2023-05-02 19:07:10 +0500

    a

--- crm/baldrcrm/client/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-from.models import Client
-class AddClientForm(forms.ModelForm):
-    class Meta:
-        model = Client
-        fields = ('name','email','description',)
